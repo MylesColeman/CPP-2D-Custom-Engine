ca267cb8f828edefb44f4afb5e44822da3eea67e
E4109732 <E4109732@TU120212>
2025-10-28 17:57:57 +0000

    started separating the simulation logic from the graphics class

--- GecProject/Graphics.h
+++ GecProject/Graphics.h
@@ -1,7 +1,7 @@
 #pragma once
-#include "Entity.h"
-#include "PlayerEntity.h"
-#include "InputManager.h"
+#include "SimulationSystem.h"
+#include <SFML/Graphics.hpp>
+#include <memory>
 
 class Graphics
 {
@@ -15,18 +15,9 @@ private:
 
 	sf::RenderWindow m_window;
 	TextureManager m_textureManager;
-	AnimationManager m_animationManager;
+	SimulationSystem m_simulation;
 
-	InputManager m_inputManager;
-
-	Entity m_zombie;
-	PlayerEntity m_player;
-
-	// Clock required by ImGui
-	sf::Clock m_uiDeltaClock;
-	// Used by ImGUI dropdown (combo) box
-	const char* m_items[4] = { "Idle", "Walk", "Attack", "Death" };
-	const char* m_current_item = "Idle";
+	sf::Clock m_uiDeltaClock; // Used by ImGui for updating
 
 	sf::Clock m_frameClock;
 	int m_frameCount{ 0 };
--- GecProject/SimulationSystem.h
+++ GecProject/SimulationSystem.h
@@ -1,8 +1,17 @@
 #pragma once
+#include "Entity.h"
+#include "PlayerEntity.h"
+#include "InputManager.h"
+
 class SimulationSystem
 {
 public:
-
+	void update();
 private:
+	AnimationManager m_animationManager;
 
+	InputManager m_inputManager;
+
+	Entity m_zombie;
+	PlayerEntity m_player;
 };
